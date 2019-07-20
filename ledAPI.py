d342b556ace04663a594a00bc6d66634aab6a637
technorainbows <i@ashleynewton.net>
2019-07-20 00:24:06 -0700

    Added jsonify and swagger documentation

--- ledAPI.py
+++ ledAPI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restplus import Api, Resource, fields
 
 
@@ -14,21 +14,29 @@ ledState = [False]
 
 @api.route('/light')
 class Light(Resource):
+    '''Returns ledState (true or false)'''
     def get(self):
-        return ledState[0]
+        return jsonify({'ledsState': ledState[0]}), 201
 
 @api.route('/light/<string:state>')
+@api.doc(params={'state':'true or false'})
 class States(Resource):
+    '''Changes ledState to on or off (true or false)'''
     def post(self, state):
         if (state=="true"):
             ledState[0] = True
-            return 200
+            return  200
         elif (state=="false"):
             ledState[0] = False
-            return 200
+            return  200
         else:
-            return 400
-        
+            # return 400
+            api.abort(404, message="{} not found".format(state))
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
  
