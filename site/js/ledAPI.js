4730589346b2efe3df27b57c31d41e1560827889
technorainbows <i@ashleynewton.net>
2019-07-22 16:15:00 -0700

    Connected postData to on/off button state change

--- site/js/ledAPI.js
+++ site/js/ledAPI.js
@@ -1,100 +1,92 @@
-var apiURL = 'http://127.0.0.1:5000/light'
+// (function(global) {
+var apiURL = 'http://lvh.me:5000/light'
 // var apiURL = 'https://jsonplaceholder.typicode.com/posts'
-console.log('ledAPI called');
-
-
-// $(document).ready(function() {
-// 	$.ajax({
-// 		url: 'apiURL',
-// 		type:"GET",
-// 		// contentType: "application/json",
-// 		// dataType: 'json'
-// 		success: function(result){
-// 		console.log(result)
-// 	},
-// 	error:function(error){
-// 		console.log('Error $(error)')
-// 	}
-
-// 	})
-// })
+console.log('ledAPI running');
 
 
 
-// const Http = new XMLHttpRequest();
-// const url = apiURL;
-// Http.open("GET", url);
-// Http.send();
-
-// Http.onreadystatechange = (e) => {
-//     console.log(Http.responseText)
-// }
+// $(document).addEventListener("DOMContentLoaded", function (event) {
+	
 
 
+// when btn-ledState is clicked, calls postData to update ledState on API
+$(document).ready(function() {
 
-// fetch('apiURL')
-// POST 
+    $("#btn-ledState :input").change(function() {
+        console.log(this.id); // points to the clicked input button
+        switch(this.id) {
+        	case "ledON":
+        		postData('true');
+        		break;
+        	case "ledOFF":
+        		postData('false');
+        		break;
+        }
 
-// postData(apiURL, {answer: 42})
-//   .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-//   .catch(error => console.error(error));
-
-// function postData(url = '', data = {}) {
-// Default options are marked with *
-// return 
-
-// fetch(apiURL, {
-//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'cors', // no-cors, cors, *same-origin
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'same-origin', // include, *same-origin, omit
-//     headers: {
-//         'Content-Type': 'application/json',
-//         // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrer: 'no-referrer', // no-referrer, *client
-//     body: JSON.stringify({'true'}), // body data type must match "Content-Type" header
-// })
-// .then(response => response.json()); // parses JSON response into native JavaScript objects 
-// }
-console.log('POST request:')
-fetch("http://lvh.me:5000/light/true", {
-        "credentials": "omit",
-        "headers": {
-            "accept": "application/json",
-            // "content-type": "application/json",
-            // "Access-Control-Allow-Origin'":"*'"
-        },
-        "referrer": "http://lvh.me:5000/",
-        "referrerPolicy": "no-referrer-when-downgrade",
-        "body": JSON.stringify('true'),
-        "method": "POST",
-        "mode": "cors",
-    })
-
-    .then(function(response) {
-        console.log(response);
-        return response.json();
-
-    })
-    .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
     });
 
 
-console.log('GET request:')
-// GET
-fetch("http://lvh.me:5000/light", { "credentials": "omit", "headers": { "accept": "application/json",
-	// "Access-Control-Allow-Origin'":"*'" 
-}, "referrer": "http://lvh.me:5000/", "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
+});
+
+// Posts state to API when called 
+function postData(state) {
+    url = apiURL + "/" + state;
+    console.log('POST request: ' + url)
+    fetch(url, {
+            "credentials": "omit",
+            "headers": {
+                "accept": "application/json",
+                // "content-type": "application/json",
+                // "Access-Control-Allow-Origin'":"*'"
+            },
+            "referrer": url,
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": JSON.stringify(state),
+            "method": "POST",
+            "mode": "cors",
+        })
+
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+        });
+
+}
+
+// Gets ledState from api
+function getData() {
+    url = apiURL;
+    console.log('GET request:' + url)
+    // GET
+    fetch(url, {
+            "credentials": "omit",
+            "headers": {
+                "accept": "application/json",
+                // "Access-Control-Allow-Origin'":"*'" 
+            },
+            "referrer": url,
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": null,
+            "method": "GET",
+            "mode": "cors"
+        })
 
 
-    .then(function(response) {
-        console.log(response);
-        return response.json();
+        .then(function(response) {
+            console.log(response);
+            return response.json();
 
-    })
-    .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-    });
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+        });
+}
+
+// postData('true');
+// getData();
+
+// })(window);
