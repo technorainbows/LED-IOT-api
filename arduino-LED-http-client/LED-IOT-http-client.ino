aef57f60bda7cdabbcfff4ee142205dc919b451e
technorainbows <i@ashleynewton.net>
2019-07-24 23:17:47 -0700

    Added ESP32 support

--- arduino-LED-http-client/LED-IOT-http-client.ino
+++ arduino-LED-http-client/LED-IOT-http-client.ino
@@ -1,5 +1,5 @@
 /*
-  LED-IOT-APP v0.0.1
+  LED-IOT-APP v0.0.2
   Simple sketch that uses ESP board to connect to local wifi network then gets LED state from python API (ledAPI.py)
 
   Receives data as JSON formatted as {"ledState": true] or {"ledState": false} and toggles LEDs between off and rainbow.
@@ -8,34 +8,47 @@
 */
 
 #include <FastLED.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
+#include "credentials.h" // wifi network credentials stored in separate file
+
+// if using ESP32
+#include <WiFiMulti.h>
+#include <WiFi.h>
 #include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <ArduinoOTA.h>
+WiFiMulti wifiMulti;
+
+// if using OLED display
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"` //OLED screen
+SSD1306Wire  display(0x3c, 5, 4); //wifi bluetooth battery oled 18650 board dispplay
+
+
+// if using ESP8266....
+//#include <ESP8266WiFi.h>
+//#include <ESP8266WiFiMulti.h>
+//#include <ESP8266HTTPClient.h>
+//ESP8266WiFiMulti wifiMulti;
 //#include <ArduinoHttpClient.h>
 
-//#include <HTTPClient.h>
 
-ESP8266WiFiMulti wifiMulti;
+
 
 String apiURL = "http://10.0.0.59:5000/light";
 String controllerURL = "10.0.0.59/site/index.html";
 
-const char * ssid1 = "_VanNest";
-const char * password1 = "alleged cat";
 
 //RMT is an ESP hardware feature that offloads stuff like PWM and led strip protocol, it's rad
 //#define FASTLED_RMT_CORE_DRIVER true
 #define FASTLED_RMT_MAX_CHANNELS 1
 FASTLED_USING_NAMESPACE
 
-#define LED_TYPE    WS2812B
-
+//#define LED_TYPE    WS2812B
+#define LED_TYPE    WS2811
 
 
 //#ifdef DEV_LIGHTCONTROL_TRIANGLE
-#define DATA_PIN   6
-//char* hostname = "ashliana";
+#define DATA_PIN   25
+char* hostname = "trianglez";
 #define COLOR_ORDER GRB //pixels
 #define COLOR_CORRECT TypicalLEDStrip
 #define NUM_LEDS 200
@@ -45,14 +58,70 @@ FASTLED_USING_NAMESPACE
 
 //#endif
 
+
 CRGB leds[NUM_LEDS];
 
+uint8_t speed = 10;
+
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+boolean connectioWasAlive = true;
+bool lastState = false;
+
+
+// Set up wireless updating if using ESP32
+void setupAOTA() {
+
+  ArduinoOTA.setHostname(hostname);
+
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+}
 
 
 void setup() {
   Serial.begin(115200);
 
+  //all this is for OLED status screen
+  display.init();
+  display.clear();
+  display.drawString(0, 0, "probably booting");
+  //display.flipScreenVertically();
+  display.display();
+
+  // set up LEDS
+  FastLED.setDither(BINARY_DITHER);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(COLOR_CORRECT);
+
+
+  // setup WIFI
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP(ssid1, password1);
   //  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
@@ -64,126 +133,187 @@ void setup() {
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+
+    // turn leds to green if connected
+    fill_solid(leds, NUM_LEDS, CRGB::Green);
+    FastLED.show();
+    delay(100);
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
   }
 
-  FastLED.setDither(BINARY_DITHER);
+  setupAOTA(); // set up arduino over-the-air updating
 
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
-  FastLED.setBrightness(BRIGHTNESS);
-
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(COLOR_CORRECT);
+  updateDisplay(); // update OLED screen on device
 
 
 }
 
-bool lastState = false;
+
+
+
+void monitorWiFi()
+{
+  if (wifiMulti.run() != WL_CONNECTED)
+  {
+    if (connectioWasAlive == true)
+    {
+      connectioWasAlive = false;
+      Serial.print("Looking for WiFi ");
+      // turn first 20 leds to red if not connected
+      fill_solid(leds, 20, CRGB::Red);
+      FastLED.show();
+    }
+    Serial.print(".");
+    delay(100);
+  }
+  else if (connectioWasAlive == false)
+  {
+    connectioWasAlive = true;
+    //    delay(500);
+    Serial.printf(" connected to %s\n", WiFi.SSID().c_str());
+    Serial.println(WiFi.localIP());
+    updateDisplay();
+
+  }
+}
+
+bool ledsState = false;
 
 void loop() {
 
-  // check wifi status
+  //   check wifi status
   if (wifiMulti.run() != WL_CONNECTED) {
     Serial.println("WiFi not connected!");
-    delay(1000);
+    // turn first 5 leds to red if not connected
+    fill_solid(leds, 20, CRGB::Red);
+    FastLED.show();
+    delay(500);
   }
 
-  // wait for WiFi connection
-  if ((wifiMulti.run() == WL_CONNECTED)) {
+  EVERY_N_MILLISECONDS(500) {
+    //    monitorWiFi();
+    ArduinoOTA.handle();
+    // wait for WiFi connection
+    if ((wifiMulti.run() == WL_CONNECTED)) {
+        
+//      Serial.println("wifi connected, connecting to api");
+      
+      HTTPClient http;
 
-    HTTPClient http;
+      // start connection and send HTTP header
 
-    // start connection and send HTTP header
+      http.begin(apiURL);
 
-    http.begin(apiURL);
+      //    Serial.print("[HTTP] GET...\n");
+      int httpCode = http.GET();
 
-    //    Serial.print("[HTTP] GET...\n");
-    int httpCode = http.GET();
+      // Check returning httpCode -- will be negative on error
+      if (httpCode > 0) {
+                    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
-    // Check returning httpCode -- will be negative on error
-    if (httpCode > 0) {
-      //            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        // file found at server
+        if (httpCode == HTTP_CODE_OK) {
+//          Serial.println("Found file at server");
+          String payload = http.getString();
+//                  Serial.println(payload);
+  
+          // parse payload
 
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        //        Serial.println(payload);
+          const size_t capacity = JSON_OBJECT_SIZE(1) + 20;
+          DynamicJsonBuffer jsonBuffer(capacity);
 
-        // parse payload
+          //        const char* json = "{\"ledsState\":true}";
 
-        const size_t capacity = JSON_OBJECT_SIZE(1) + 20;
-        DynamicJsonBuffer jsonBuffer(capacity);
+          JsonObject& root = jsonBuffer.parseObject(payload);
 
-        //        const char* json = "{\"ledsState\":true}";
+           ledsState = root["ledsState"]; // true
 
-        JsonObject& root = jsonBuffer.parseObject(payload);
-
-        bool ledsState = root["ledsState"]; // true
-
-        if (ledsState != lastState) {
-          lastState = ledsState;
-          updateLEDS(ledsState);
-          Serial.printf("ledState = %d\n", ledsState);
+          if (ledsState != lastState) {
+            lastState = ledsState;
+            Serial.printf("ledState = %d\n", ledsState);
+            
+          }
 
         }
-
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
-    } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
 
-    http.end(); // close connection
+      http.end(); // close connection
+//      Serial.println("Closing connection");
+    }
   }
 
-  delay(500);
-}
-
-
-void updateLEDS(bool state) {
-
-  if (state) fill_rainbow( leds, NUM_LEDS, gHue);
-
-  else fill_solid(leds, NUM_LEDS, CRGB::Black);
-
+//  Serial.println("FastLED.show & FastLED.delay");
+  //  delay(500);
+  updateLEDS();
   FastLED.show();
 
+  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000 / FRAMES_PER_SECOND);
 
+}
 
-  // do some periodic updates
-  //        EVERY_N_MILLISECONDS( 20 ) {
-  //        gHue++;  // slowly cycle the "base color" through the rainbow
-  //        }
-  //  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+
+void updateLEDS() {
+
+  if (!ledsState) fill_solid(leds, NUM_LEDS, CRGB::Black);
+  
+  else {
+    fill_rainbow( leds, NUM_LEDS, gHue, speed);
+    EVERY_N_MILLISECONDS( 10 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  }
+    
+
+
 
 }
 
-//
-//bool readReponseContent(struct UserData* userData) {
-//  // Compute optimal size of the JSON buffer according to what we need to parse.
-//  // See https://bblanchon.github.io/ArduinoJson/assistant/
-//  const size_t BUFFER_SIZE =
-//    JSON_OBJECT_SIZE(8)    // the root object has 8 elements
-//    + JSON_OBJECT_SIZE(5)  // the "address" object has 5 elements
-//    + JSON_OBJECT_SIZE(2)  // the "geo" object has 2 elements
-//    + JSON_OBJECT_SIZE(3)  // the "company" object has 3 elements
-//    + MAX_CONTENT_SIZE;    // additional space for strings
-//
-//  // Allocate a temporary memory pool
-//  DynamicJsonBuffer jsonBuffer(BUFFER_SIZE);
-//
-//  JsonObject& root = jsonBuffer.parseObject(client);
-//
-//  if (!root.success()) {
-//    Serial.println("JSON parsing failed!");
-//    return false;
-//  }
-//
-//  // Here were copy the strings we're interested in
-//  strcpy(userData->name, root["name"]);
-//  strcpy(userData->company, root["company"]["name"]);
-//  // It's not mandatory to make a copy, you could just use the pointers
-//  // Since, they are pointing inside the "content" buffer, so you need to make
-//  // sure it's still in memory when you read the string
-//
-//  return true;
-//}
+
+
+
+void updateDisplay() {//updates the little OLED status screen
+
+  display.clear();
+  display.setFont(ArialMT_Plain_24); //11,16,24
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 0, hostname);
+  //display.display(); //draw the screen
+
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 22, WiFi.localIP().toString());
+  //  display.display(); //draw the screen
+
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 36, WiFi.SSID().c_str());
+  display.drawString(0, 46, String(FastLED.getFPS()) + "FPS");
+
+  display.display(); //draw the screen
+
+
+}
+/*
+
+
+      Built upon the amazing FastLED work of Daniel Garcia and Mark Kriegsman:
+   https://github.com/FastLED/FastLED
+
+   ESP32 support provided by the hard work of Sam Guyer:
+   https://github.com/samguyer/FastLED
+
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
