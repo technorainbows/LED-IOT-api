bec09fa7572e68f6354f305fc982d6e220d143ae
technorainbows <i@ashleynewton.net>
2019-08-15 18:49:22 -0700

    add redis class

--- ledAPI.py
+++ ledAPI.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask import Flask, jsonify, request, json
 from flask_restplus import Api, Resource, fields, cors
 # from werkzeug.contrib.fixers import ProxyFix
+import redis
+import json
 
 app = Flask(__name__)
 # app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -17,17 +19,20 @@ CORS(app)
 DEVICES = {
     'device1': 
         {
-            'onState': False, 
+            'onState': 'False', 
             'brightness': 200,
             'name':'Ashley-Triangle'
         },
     'device2': 
         {
-            'onState': True, 
+            'onState': 'True', 
             'brightness': 100,
             'name':'Other'
         }
 }
+
+
+
 
 '''Single Device Data Model'''
 device = api.model('Device', {
@@ -49,8 +54,91 @@ def abort_if_device_not_found(device_id):
 
 
 
-# parser = api.parser()
-# parser.add_argument('device',  required=True, help='The device details', location='form')
+# ''''''''''''
+# '''REDIS''''
+# ''''''''''''
+
+
+# r.hmset('device1',DEVICES['device1'])
+# r.hmset('device2',DEVICES['device2'])r =
+
+# rDevices1 = r.hgetall('device1')
+# print('rDevice1s: ', rDevices1)
+# rDevices2 = r.hgetall('device2')
+# print('rDevice2s: ', rDevices2)
+
+
+
+# print(r.hgetall('device1'))
+# print(r.hgetall('device2'))
+
+# '''Initial Write of DEVICES data to REDIS'''
+# with r.pipeline() as pipe:
+#     for device_id, device in DEVICES.items():
+#         pipe.hmset(device_id,device)
+#     pipe.execute()
+# r.bgsave()
+
+# LUA_HMGET =  """
+#     if redis.call('EXISTS', KEYS[1]) == 1 then
+#         return redis.call('HGETALL', KEYS[1])
+#     else
+#         redis.call('HMSET', KEYS[1], ARGV[1] ARGV[2])
+#         return ARGV[2]
+#     end
+#     """
+
+'''
+lua script gets device settings from Redis if there, otherwise 
+initializes device settings to default
+'''
+
+LUA_GET =  """
+    if redis.call('EXISTS', KEYS[1]) == 1 then
+        return redis.call('GET', KEYS[1])
+    else
+        redis.call('SET', KEYS[1], ARGV[1])
+        return ARGV[1]
+    end
+    """
+
+
+
+class Redis(object):
+    def __init__(self):
+        self.redis = redis.Redis(host='localhost', port=6379, db=0)
+        '''
+        Initialize script object - The Script object ensures that the LUA script is loaded into Redis's script cache. 
+        In the event of a NOSCRIPT error, it will load the script and retry executing it.
+        '''
+        self.getDevice = self.redis.register_script(LUA_GET)
+
+
+        '''if 'device1' on redis server, load it with default info'''
+        self.getDevice(keys=['device1'],args=[json.dumps(DEVICES['device1'])])
+
+
+    
+    def set(self,key,data):
+       self.redis.set(key,json.dumps(data))
+       return self.get(key)
+
+    def get(self,key):
+        # load lua script on redis server
+        # execute lua script (with 'key' as argument)
+        # Atomic:(check if device info in redis
+        # if not, load the default, and then return it)
+        # assert r.script_exists(initDevice)
+        # for key, device in 
+        data = self.getDevice(keys=[key],args=[json.dumps(DEVICES[key])])
+
+        return json.loads(data)
+
+
+    # def delete(self,key):
+
+'''Initialize REDIS object'''
+REDIS = Redis()
 
 
 ''''''''''''''''''''''''''''''''''''
@@ -62,14 +150,15 @@ def abort_if_device_not_found(device_id):
 class Device(Resource):
     '''Show a single device's properties and lets you delete them or change them'''
     @api.response(200, 'Success', device)
-    # @cors.crossdomain(origin='*', headers='content-type', methods=['get'])
     def get(self, device_id):
         '''Fetch a given resource'''
         abort_if_device_not_found(device_id)
-        return jsonify(DEVICES[device_id],200)
+        redisGet = REDIS.get(device_id)
+        print('redisGet: ', redisGet)
+        return jsonify(redisGet)
+        # return jsonify(DEVICES[device_id],200)
         
     @api.doc(responses={204: 'Device deleted'})
-    # @cors.crossdomain(origin='*', headers=['content-type'], methods=['delete'])
     def delete(self, device_id):
         '''Delete a given resource'''
         abort_if_device_not_found(device_id)
@@ -79,8 +168,6 @@ class Device(Resource):
     @api.expect(device, validate=True)
     # @api.doc(parser=parser)
     @api.response(200, 'Success', device)
-    # @cors.crossdomain(origin='*',headers=['content-type'],methods=['PUT','OPTIONS'],)
-    # @cross_origin(allow_headers=['content-type'])
     def put(self, device_id):
         '''Update a given resource's onState or brightness properties'''
         if 'onState' in request.json:
@@ -98,8 +185,11 @@ class Device(Resource):
             DEVICES[device_id]['name']=newbrightness
             print('new name stored: ', DEVICES[device_id]['name'])
 
-        print(DEVICES[device_id])
-        return jsonify(DEVICES[device_id])
+        # print(DEVICES[device_id])
+        redisSet = REDIS.set(device_id, DEVICES[device_id])
+        print('redisSet = ', redisSet)
+        return jsonify(redisSet)
+        # return jsonify(DEVICES[device_id])
 
 
 ''''''''''''''''''''''''''''''''''''''
@@ -109,7 +199,6 @@ class Device(Resource):
 class DeviceList(Resource):
     '''Shows a list of all devices, and lets you POST to add new tasks'''
     @api.response(200, 'Success', list_of_devices)
-    # @cors.crossdomain(origin='*', headers=['content-type'], methods='get')
     def get(self):
         '''Return a list of all devices and their properties'''
         print("returning Device List: ", DEVICES)
@@ -118,7 +207,6 @@ class DeviceList(Resource):
                  # 200)
 
     @api.expect(device, validate=True)
-    # @cors.crossdomain(origin='*', headers=['content-type'], methods=['post'],automatic_options=False)
     def post(self):
         '''Create a new device with next id'''       
         device_id = 'device%d' % (len(DEVICES) + 1)
@@ -136,19 +224,15 @@ class DeviceList(Resource):
             DEVICES[device_id]['name']=request.json.get('name',DEVICES[device_id]['name'])
             print('name found')
             
-        print(DEVICES[device_id])
-        return jsonify(DEVICES[device_id], 201)
+        # print(DEVICES[device_id])
+        redisSet = REDIS.set(device_id, DEVICES[device_id])
+        print('redisSet = ', redisSet)
+        return jsonify(redisSet)
+        # return jsonify(DEVICES[device_id], 201)
 
 @app.errorhandler(404)
 def not_found(error):
     return (jsonify({'error': 'Not found'}), 404)
-
-'''Add headers to bypass CORS issues'''
-# @app.after_request
-# def after_request(response):
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
 
 
 if __name__ == '__main__':
