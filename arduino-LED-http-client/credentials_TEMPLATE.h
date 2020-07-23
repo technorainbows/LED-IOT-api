bc911af14745c5d7e2d99b81ffcc407d7ec196ad
technorainbows <i@ashleynewton.net>
2020-07-23 15:15:41 -0700

    Create example template for certificate.h

---
+++ arduino-LED-http-client/credentials_TEMPLATE.h
@@ -0,0 +1,16 @@
+// Store as many network credentials as you desire here.
+
+const char *ssid1 = "INSERT_SSID1";
+const char *password1 = "password1";
+
+const char *ssid2 = "INSERT_SSID2";
+const char *password2 = "password2";
+
+const char *ssid3 = "INSERT_SSID3";
+const char *password3 = "password3";
+
+// get auth token from authentication provider or through logging in.
+String auth_token = "INSERT_AUTH_TOKEN_HERE";
+
+// get rootCACertificate from host
+const char *rootCACertificate = "INSERT_CERTIFICATE_HERE;
